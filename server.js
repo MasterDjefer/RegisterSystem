b5de248aa79e98052eff82f72a37eff7ae2d9d1e
MasterDjefer <kostia210@gmail.com>
2018-11-09 13:08:02 +0200

    add register page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let users =
         password: "admin"
     }
 ];
-
+ 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "\\public"));
@@ -28,6 +28,12 @@ app.get("/openLogIn", function(req, res)
     console.log("/openLogIn");
     res.render("login");
 });
+app.get("/openRegister", function(req, res) 
+{
+    console.log("/openRegister");
+    res.render("register");
+});
+
 app.post("/profile", function(req, res) 
 {
     console.log("/profile");
@@ -47,7 +53,42 @@ app.post("/profile", function(req, res)
     }
     else
     {
-        res.redirect("openLogIN");
+        res.redirect("openLogIn");
+    }
+});
+app.post("/register", function(req, res) 
+{
+    console.log("/register");
+
+    if ((req.body.password1 == "" || req.body.password2 == "" || req.body.userName == "") || 
+        (req.body.password1 != req.body.password2))
+    {
+        res.redirect("/openRegister");
+        return;
+    }
+    let user = 
+    {
+        userName: req.body.userName,
+        password: req.body.password1
+    };
+
+    let findUser = users.find(function(element)
+    {
+        if (element.userName == user.userName)
+        {
+            return true;
+        }
+        return false;
+    });
+
+    if (!findUser)
+    {
+        users.push(user);
+        res.redirect("/");
+    }
+    else
+    {
+        res.redirect("/openRegister");
     }
 });
 
